e188b4921701b4cc816b6e06493bffcd26a5d3c4
Christine <christinecheng1229@gmail.com>
2025-11-17 18:36:47 -0500

    API result GUI in progress

--- src/main/java/use_case/search/ByLanguage/SearchByLanguage.java
+++ src/main/java/use_case/search/ByLanguage/SearchByLanguage.java
@@ -1,4 +1,14 @@
 package use_case.search.ByLanguage;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class SearchByLanguage {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+
+            JFrame frame = new JFrame("Search Results");
+        });
+    }
 }
