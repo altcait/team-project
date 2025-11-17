12ccf22de2ecf92b8c1e0fb5de9d1b638d89992d
Maria Fernanda Reyes <mariafreyes05@gmail.com>
2025-11-17 17:22:54 -0500

    added the login classes and interfaces (base code) and created User entity

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,20 @@
+package entity;
+
+public class User {
+
+    private final String name;
+    private final String password;
+
+    public User(String name, String password) {
+        if ("".equals(name)) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        if ("".equals(password)) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+        this.name = name;
+        this.password = password;
+    }
+    public String getName() {return name;}
+    public String getPassword() {return password;}
+}
---
+++ src/main/java/use_case/login/LoginInteractor.java
@@ -0,0 +1,8 @@
+package use_case.login;
+
+import entity.User;
+
+public class LoginInteractor {
+
+
+}
