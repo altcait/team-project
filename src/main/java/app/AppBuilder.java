cec30d6aa85f200399f0f11b445a8f1072167939
Maria Fernanda Reyes <mariafreyes05@gmail.com>
2025-11-18 18:10:10 -0500

    login view working so far

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -1,4 +1,49 @@
 package app;
 
+import entity.UserFactory;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.login.LoginController;
+import interface_adapter.login.LoginPresenter;
+import interface_adapter.login.LoginViewModel;
+import use_case.login.LoginInputBoundary;
+import use_case.login.LoginInteractor;
+import use_case.login.LoginOutputBoundary;
+import view.LoginView;
+import view.ViewManager;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class AppBuilder {
+    private final JPanel cardPanel = new JPanel();
+    private final CardLayout cardLayout = new CardLayout();
+    final UserFactory userFactory = new UserFactory();
+    final ViewManagerModel viewManagerModel = new ViewManagerModel();
+    ViewManager viewManager = new ViewManager(cardPanel, cardLayout, viewManagerModel);
+
+    private LoginView loginView;
+    private LoginViewModel loginViewModel;
+
+    public AppBuilder() {
+        cardPanel.setLayout(cardLayout);
+    }
+
+    public AppBuilder addLoginView() {
+        loginViewModel = new LoginViewModel();
+        loginView = new LoginView(loginViewModel);
+        cardPanel.add(loginView, loginView.getViewName());
+        return this;
+    }
+
+    public JFrame build() {
+        final JFrame application = new JFrame("User Login Example");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        application.add(cardPanel);
+
+        viewManagerModel.setState((loginView).getViewName());
+        viewManagerModel.firePropertyChange();
+
+        return application;
+    }
 }
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         AppBuilder appBuilder = new AppBuilder();
         JFrame application = appBuilder
-
+                .addLoginView().build();
 
         application.pack();
         application.setLocationRelativeTo(null);
